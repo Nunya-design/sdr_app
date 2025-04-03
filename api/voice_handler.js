358d6888760d8db272ec64a232683b85cc1a444b
Nunya-design <waltlafky@gmail.com>
2025-04-03 13:46:03 -0700

    Update voice_handler.js

--- api/voice_handler.js
+++ api/voice_handler.js
@@ -1,1 +1,12 @@
+const twilio = require('twilio');
+
+module.exports = (req, res) => {
+  const twiml = new twilio.twiml.VoiceResponse();
+
+  twiml.say("Hi there! This is Sarah from Plenty. Thanks for taking the call.");
+  twiml.hangup();
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.status(200).send(twiml.toString());
+};
 
